08ea8f5760c17afbf5db2087bdf763bd939b3907
Braian Zignago <braianzig77@gmail.com>
2025-09-25 21:06:11 -0300

    First Commit

---
+++ src/main/java/dev/java10x/MagicFrigdeIA/MagicFrigdeIaApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.MagicFrigdeIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicFrigdeIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicFrigdeIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/MagicFrigdeIA/MagicFrigdeIaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.MagicFrigdeIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicFrigdeIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
